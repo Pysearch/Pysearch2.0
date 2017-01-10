7a74f0d034840d12b82e4bb75c6f6e3a4e2e4317
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-01-10 12:42:38 -0800

    edited template for results, edited views results list of dicts, added temp test_harv.py.

---
+++ pysearch/scripts/test_harv.py
@@ -0,0 +1,4 @@
+"""From pysearch dir. Same level as routes.py.'python scripts/test_harv.py'."""
+# from pysearch.harvester.spiders.crawler import crawl
+
+# crawl()
--- pysearch/views/default.py
+++ pysearch/views/default.py
@@ -7,6 +7,7 @@ from sqlalchemy.exc import DBAPIError
 from pyramid.httpexceptions import HTTPFound
 from ..models import Keyword
 from pysearch.harvester.spiders.harvester import harvest
+from pysearch.harvester.spiders.crawler import crawl
 
 
 @view_config(route_name='home', renderer='../templates/home.jinja2')
@@ -23,18 +24,19 @@ def home_view(request):
 def computing_results_view(request):
     """Remove authentication from the user."""
     # import pdb; pdb.set_trace()
+    crawl()
     return HTTPFound(request.route_url("results"))
 
 
 RESULTS = [
-    {'keyword': 'applepie', 'keyword_weight': 4, 'title_urls': '', 'header_url': '', 'body_url': 'https://www.pillsbury.com/'},
-    {'keyword': 'applepie', 'keyword_weight': 3, 'title_urls': 'http://www.bettycrocker.com/recipes/', 'header_url': '', 'body_url': ''},
-    {'keyword': 'applepie', 'keyword_weight': 2, 'title_urls': '', 'header_url': '', 'body_url': 'http://allrecipes.com/recipe/12682/'},
-    {'keyword': 'applepie', 'keyword_weight': 2, 'title_urls': '', 'header_url': 'http://allrecipes.com/recipe/12682/', 'body_url': ''},
-    {'keyword': 'applepie', 'keyword_weight': 1, 'title_urls': '', 'header_url': '', 'body_url': 'https://www.applepie.com'},
-    {'keyword': 'applepie', 'keyword_weight': 5, 'title_urls': 'https://www.google.com/apple_pie', 'header_url': '', 'body_url': ''},
-    {'keyword': 'applepie', 'keyword_weight': 5, 'title_urls': '', 'header_url': 'https://www.google.com/apple_pie', 'body_url': ''},
-    {'keyword': 'applepie', 'keyword_weight': 3, 'title_urls': '', 'header_url': '', 'body_url': 'http://www.bettycrocker.com/recipes/'}
+    {'keyword': 'applepie', 'keyword_weight': '4', 'title_urls': 'http://www.bettycrocker.com/recipes/', 'header_urls': 'https://www.pillsbury.com/', 'body_urls': 'https://www.pillsbury.com/'},
+    {'keyword': 'applepie', 'keyword_weight': '3', 'title_urls': 'http://www.bettycrocker.com/recipes/', 'header_urls': 'https://www.pillsbury.com/', 'body_urls': 'https://www.pillsbury.com/'},
+    {'keyword': 'applepie', 'keyword_weight': '2', 'title_urls': 'http://www.bettycrocker.com/recipes/', 'header_urls': 'https://www.pillsbury.com/', 'body_urls': 'http://allrecipes.com/recipe/12682/'},
+    {'keyword': 'applepie', 'keyword_weight': '2', 'title_urls': 'http://www.bettycrocker.com/recipes/', 'header_urls': 'http://allrecipes.com/recipe/12682/', 'body_urls': 'https://www.pillsbury.com/'},
+    {'keyword': 'applepie', 'keyword_weight': '1', 'title_urls': 'http://www.bettycrocker.com/recipes/', 'header_urls': 'https://www.pillsbury.com/', 'body_urls': 'https://www.applepie.com'},
+    {'keyword': 'applepie', 'keyword_weight': '5', 'title_urls': 'https://www.google.com/apple_pie', 'header_urls': 'https://www.pillsbury.com/', 'body_urls': 'https://www.pillsbury.com/'},
+    {'keyword': 'applepie', 'keyword_weight': '5', 'title_urls': 'http://www.bettycrocker.com/recipes/', 'header_urls': 'https://www.google.com/apple_pie', 'body_urls': 'https://www.pillsbury.com/'},
+    {'keyword': 'applepie', 'keyword_weight': '3', 'title_urls': 'http://www.bettycrocker.com/recipes/', 'header_urls': 'https://www.pillsbury.com/', 'body_urls': 'http://www.bettycrocker.com/recipes/'}
 ]
 
 
