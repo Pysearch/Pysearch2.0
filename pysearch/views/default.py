c368ca03c175ad670be5f532f4640faabba847ab
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-01-12 17:02:47 -0800

    debugging asking simple db query results view page.

--- pysearch/views/default.py
+++ pysearch/views/default.py
@@ -56,18 +56,18 @@ def computing_results_view(request):
 @view_config(route_name='results', renderer='../templates/results.jinja2')
 def results_view(request):
     """Append result of each unique keyword of each unique url to be passed to be scored."""
-    # results = []
-    # try:
-    #     # results = query.filter(Keyword.keyword == 'applepie1')
+    results = []
+    try:
+        # results = query.filter(Keyword.keyword == 'applepie1')
 
-    #     """
-    #     Set results to query.all() to render Keyword model data on results page.
-    #     """
-    #     url = request.params["url"]
-    #     unique_urls = []
-    #     for val in request.dbsession.query(Match.page_url).distinct():
+        """
+        Set results to query.all() to render Keyword model data on results page.
+        """
+        url = request.params["url"]
+        unique_urls = []
+        for val in request.dbsession.query(Match.page_url).distinct():
 
-    #         unique_urls.append(val[0])
+            unique_urls.append(val[0])
 
     #     print(unique_urls)
 
@@ -82,8 +82,8 @@ def results_view(request):
     #             if url_q:
     #                 results.append({'keyword': kw, 'weight': url_q.keyword_weight, 'url': url, 'count': url_q.count})
 
-    # except DBAPIError:
-    #     return Response(db_err_msg, content_type='text/plain', status=500)
+    except DBAPIError:
+        return Response(db_err_msg, content_type='text/plain', status=500)
     # results = score_data(results)
     # return {"RESULTS": results, "URL": url}
     return {}
